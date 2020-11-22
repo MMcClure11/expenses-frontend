af140d89576a99e3f7d91af9261a72f9a0c72880
MMcClure11 <mcclme02@luther.edu>
2020-11-22 16:15:37 -0600

    makes successful fetch request to backend to populate store with accounts array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import {fetchAccounts} from '../actions/fetchAccounts'
+import {fetchAccounts} from './actions/fetchAccounts'
 
 
 class App extends React.Component {
