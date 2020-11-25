3bd869c5c351fb2ca9c11648294fd3b9a45bd7ab
MMcClure11 <mcclme02@luther.edu>
2020-11-25 14:28:53 -0600

    refactors to be at same place as Annabel

--- src/components/Account.js
+++ src/components/Account.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Account = (props) => {
 
   let account = props.accounts[props.match.params.id - 1]
-
+  
   return (
     <div>
-      <h2>
+      <li>
         {account ? account.name : null} - {account ? account.balance : null}
-      </h2>
+      </li>
     </div>
   )
 }
--- src/components/Accounts.js
+++ src/components/Accounts.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Account from './Account'
 
 const Accounts = props => {
+
+  // let accountFirst = props.accounts.map(account => account.name)
+  // console.log(accountFirst)
 
   return (
     <div>
       <h1>Accounts</h1>
-      {props.accounts.map(account => <li key={account.id}>{account.name} - {account.balance}</li>)}
+      {props.accounts.map(account => 
+      <div key={account.id}><Account accountProp={account} /></div>)}
     </div>
   )
 }
